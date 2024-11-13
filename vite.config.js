0376b9355f87cb6399ef55e85bc8b1cfd848fbdf
Hugo-17 <hugo_mlopezdelanieta@alumnos-santagema.es>
2024-11-13 17:58:32 +0100

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/my-proyect-code-pen',
-    plugins: [vue()]
-})
+  plugins: [react()],
+  base: "/my-code-link/", // YOUR REPO NAME HERE
+});
