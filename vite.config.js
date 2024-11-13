d53bc522a09dbe06b05d88d086f54691dc811199
Hugo-17 <hugo_mlopezdelanieta@alumnos-santagema.es>
2024-11-13 18:04:04 +0100

    add deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 // vite.config.ts
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vue()],
   base: "/my-code-link/", // YOUR REPO NAME HERE
 });
